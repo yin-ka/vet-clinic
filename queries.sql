d4ad2bd39353ff2b39c67fb4369bce8e922ddf2f
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-08-23 23:27:37 +0100

    Update queries file with all query transactions

--- queries.sql
+++ queries.sql
@@ -14,7 +14,7 @@ SELECT * FROM animals WHERE name <> 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.40 AND 17.30;
 
--- Update and Delete
+-- Update and Delete changes to species
 BEGIN;
 
 UPDATE animals SET species = 'unspecified';
@@ -22,3 +22,40 @@ UPDATE animals SET species = 'unspecified';
 SELECT * FROM animals;
 
 ROLLBACK;
+
+-- Setting animals species
+BEGIN;
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+SELECT * FROM animals;
+
+COMMIT;
+
+SELECT * FROM animals;
+
+-- Delete and rollback table;
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+
+-- Savepoint
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT SP1;
+
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO SAVEPOINT SP1;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT;
